f1bbf3ecfff7f7246d9feb704e7926bf7f19ae88
Sanapalli Lokesh <hi@lokesh1729.com>
2021-04-14 22:41:17 +0530

    adding stdin for crooked dice and number of turns, separated it to a different function

--- main.py
+++ main.py
@@ -1,29 +1,37 @@
 import random
 
-def main():
-    print("Crooked Dice ? (Y/N)")
-    is_crooked_dice = False
-    if input().strip().lower() == 'y':
-        is_crooked_dice = True
-    turns = 10
+def run_game(turns, is_crooked_dice):
     position = 1
     snakes = [(97, 62), (77, 33), (54, 28), (38,11)]
     while turns > 0 and position < 100 :
-        try:
-            if is_crooked_dice:
-                number = random.randint(1,6)
-            else:
-                number = random.choice([2,4,6])
-            position += number
-            snake_on_path = next(filter(lambda item: item[0] == position, snakes), None)
-            if snake_on_path is not None:
-                position = snake_on_path[1]
-        except ValueError:
-            print("please enter a valid input! only positive")
-    if position == 100:
+        if is_crooked_dice:
+            number = random.randint(1,6)
+        else:
+            number = random.choice([2,4,6])
+        position += number
+        if position > 100:
+            break
+        snake_on_path = next(filter(lambda item: item[0] == position, snakes), None)
+        if snake_on_path is not None:
+            print("Biten by snake, jumping down from %s to %s" % (position, snake_on_path[1]))
+            position = snake_on_path[1]
+        turns -= 1
+    if position >= 100:
         print("You win! congrats!")
     else:
         print("game ended! you reached %s" % position)
+
+def main():
+    print("Crooked Dice ? (Y/N) : ", end="")
+    is_crooked_dice = False
+    if input().strip().lower() == 'y':
+        is_crooked_dice = True
+    print("Enter number of turns for the game to run : ", end="")
+    try:
+        turns = int(input().strip())
+    except ValueError:
+        turns = 10
+    run_game(turns, is_crooked_dice)
 
 
 if __name__ == "__main__":
