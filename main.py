2009df266474418f668a8dbe9354225122f25f21
Sanapalli Lokesh <hi@lokesh1729.com>
2021-04-14 22:27:29 +0530

    added snakes to the board

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 def main():
     turns = 10
-    position = 0
-    while turns > 0 & position < 100 :
+    position = 1
+    snakes = [(97, 62), (77, 33), (54, 28), (38,11)]
+    while turns > 0 and position < 100 :
         try:
             number = int(input().strip())
             position += number
+            snake_on_path = next(filter(lambda item: item[0] == position, snakes), None)
+            if snake_on_path is not None:
+                position = snake_on_path[1]
         except ValueError:
             print("please enter a valid input! only positive")
-        
-    print("game ended!")
+    if position == 100:
+        print("You win! congrats!")
+    else:
+        print("game ended! you reached %s" % position)
 
 
 if __name__ == "__main__":
